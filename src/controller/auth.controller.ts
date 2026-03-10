3c5f5981a915a33b116184cd8b9d865c1f974010
jihad <codebyjihad@gmail.com>
2026-03-10 19:19:14 +0600

    build: Configure project for TypeScript development
    - Add dev, build, and start scripts
    - Update main entry point to dist/server.js
    - Correct auth controller import path
    - Add project

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express"
-import bcrypt from "bcrypt"
+import  bcrypt from "bcrypt"
 import User from "../models/User"
 import { generateToken } from "../utils/jwt"
 
